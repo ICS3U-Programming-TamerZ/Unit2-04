4c0fc16b95338e5600972536405a05a6531888cf
Tamer Zreg <tamer.zreg@stu.oscb.ca>
2022-10-02 01:02:02 +0000

    added pizza_cost.py and constants.py

---
+++ pizza_cost.py/constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Created by: Tamer Zreg
+# Created on: Sep 29, 2022
+# This program has the constants
+
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGRED_COST = 1.5
